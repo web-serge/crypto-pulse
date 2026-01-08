a988385ef4e71a486822d8565b1784c5efdb921f
Sergey Churekov <mysergemail@gmail.com>
2026-01-08 21:28:54 +0300

    dev-1: добавил i18n, header, базовые стили

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
 };
 
-export default nextConfig;
+const withNextIntl = createNextIntlPlugin({
+  experimental: {
+    // Provide the path to the messages that you're using in `AppConfig`
+    createMessagesDeclaration: "./messages/en.json",
+  },
+});
+
+export default withNextIntl(nextConfig);
